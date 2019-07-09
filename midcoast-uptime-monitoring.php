eb59326891cbf6cf4616eadd9017dc03fd8cbd83
Caleb Zahnd <caleb@midcoast.io>
2019-07-09 15:41:59 -0500

    Switching to Plugin Update Checker

--- midcoast-uptime-monitoring.php
+++ midcoast-uptime-monitoring.php
@@ -34,21 +34,15 @@ function hook_uptimemonitor() {
 }
 add_action('wp_head', 'hook_uptimemonitor');
 
-include_once('updater.php');
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/midcoast-collective/Midcoast-Uptime-Monitoring/',
+	__FILE__,
+	'midcoast-uptime-monitoring'
+);
 
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-	$config = array(
-		'slug' => 'midcoast-uptime-monitoring', // this is the slug of your plugin
-		'proper_folder_name' => 'midcoast-uptime-monitoring', // this is the name of the folder your plugin lives in
-		'api_url' => 'https://api.github.com/repos/midcoast-collective/Midcoast-Uptime-Monitoring', // the GitHub API url of your GitHub repo
-		'raw_url' => 'https://raw.github.com/midcoast-collective/Midcoast-Uptime-Monitoring/master', // the GitHub raw url of your GitHub repo
-		'github_url' => 'https://github.com/midcoast-collective/Midcoast-Uptime-Monitoring', // the GitHub url of your GitHub repo
-		'zip_url' => 'https://github.com/midcoast-collective/Midcoast-Uptime-Monitoring/zipball/master', // the zip url of the GitHub repo
-		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-		'requires' => '3.0', // which version of WordPress does your plugin require?
-		'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-		'readme' => 'README.md', // which file to use as the readme for the version number
-		'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
-	);
-	new WP_GitHub_Updater($config);
-}
+//Optional: If you're using a private repository, specify the access token like this:
+//$myUpdateChecker->setAuthentication('your-token-here');
+
+//Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('master');
---
+++ readme.txt
@@ -0,0 +1,10 @@
+=== Midcoast Uptime Monitoring ===
+Contributors: calebzahnd
+Requires at least: 4.6
+Tested up to: 4.7
+Stable tag: 4.3
+Requires PHP: 5.2.4
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin writes a custom meta tag in the site header for use in UptimeMonitor.com
